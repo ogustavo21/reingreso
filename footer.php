952a5b0b4599dfd2fc44ba6c1e5bc1ad6d51ca60
jasolanv <diab.mov@gmail.com>
2020-07-08 09:41:28 -0700

    eliminar archivos

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-                    <div class="">
-                        <p class="pull-right">Gentelella Alela! a Bootstrap 3 template by <a>Kimlabs</a>. |
-                            <span class="lead"> <i class="fa fa-paw"></i> Universidad de Navojoa</span>
-                        </p>
-                    </div>
-                    <div class="clearfix"></div>
-                </footer>
