cd1345ef70ccc335a9ccd06353809c75baac3e25
Gustavo Ordoñez Nuñez <ogustavo@unav.edu.mx>
2020-07-10 16:55:56 -0700

    inserta carrera

--- registro.php
+++ registro.php
@@ -2,8 +2,6 @@
    // include_once("session.php");
     require("utils/conexion.php");
 
-    /*$query1="INSERT INTO pasos_r (id_pasos, id_reingreso,idPaso_r,estatus) VALUES ('',$id_reingreso,2,1)";
-    $result = $conexion->query($query1);*/
 session_start();
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -41,6 +39,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
                     foreach ($obj as $key => $value) {
                         echo $nombreAcademica=$obj[$key]->NOMBRE;
+                        echo $carreraAca=$obj[$key]->CARRERA_ID;
                         echo "<br>";
                     }
 
@@ -50,8 +49,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                               </script>';
                                 header("location: login.php?error='No existe'");
                     }else{
+                            $idcarrera=carrera($carreraAca);
 
-                            $query ="INSERT INTO reingreso_r(id_reingreso, nombre, a_paterno, a_materno, correo, telefono, contrasena,  estatus, tipo, matricula) VALUES ('', '$nombre', '$paterno', '$materno', '$correo', '$telefono', '$contrasena', 1, 'Estudiante', '$matricula')";
+                            $query ="INSERT INTO reingreso_r(id_reingreso, nombre, a_paterno, a_materno, correo, telefono, contrasena,  estatus, tipo, id_carrera, matricula) VALUES ('', '$nombre', '$paterno', '$materno', '$correo', '$telefono', '$contrasena', 1, 'Estudiante', '$idcarrera','$matricula')";
                             $result = $conexion->query($query);
 
                             if ($result)      
@@ -81,6 +81,124 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
    }//request
 
 
+function carrera($idcarrera){
+     $carrera=0;
+ switch ($idcarrera) {
+      
+case 20802:
+            $carrera=1;
+            break;
+case 20101:
+            $carrera=5;
+            break;
+case 20203:
+            $carrera=11;
+            break;
+case 20204:
+            $carrera=11;
+            break;
+case 20201:
+            $carrera=11;
+            break;
+case 20202:
+            $carrera=11;
+            break;
+case 20301:
+            $carrera=10;
+            break;
+case 20409:
+            $carrera=20;
+            break;
+case 20405:
+            $carrera=17;
+            break;
+case 20404:
+            $carrera=16;
+            break;
+case 20403:
+            $carrera=15;
+            break;
+case 20402:
+            $carrera=19;
+            break;
+case 20401:
+            $carrera=18;
+            break;
+case 20601:
+            $carrera=6;
+            break;
+case 20901:
+            $carrera=26;
+            break;
+case 20904:
+            $carrera=26;
+            break;
+case 20902:
+            $carrera=26;
+            break;
+case 20903:
+            $carrera=26;
+            break;
+case 20905:
+            $carrera=26;
+            break;
+case 20906:
+            $carrera=26;
+            break;
+case 21201:
+            $carrera=8;
+            break;
+case 21301:
+            $carrera=25;
+            break;
+case 20215:
+            $carrera=24;
+            break;
+case 20208:
+            $carrera=13;
+            break;
+case 20205:
+            $carrera=13;
+            break;
+case 20207:
+            $carrera=13;
+            break;
+case 20206:
+            $carrera=13;
+            break;
+case 20410:
+            $carrera=12;
+            break;
+case 20408:
+            $carrera=12;
+            break;
+case 20407:
+            $carrera=14;
+            break;
+case 20406:
+            $carrera=13;
+            break;
+case 20703:
+            $carrera=4;
+            break;
+case 20702:
+            $carrera=4;
+            break;
+case 20701:
+            $carrera=4;
+            break;
+case 20704:
+            $carrera=4;
+            break;
+case 20801:
+            $carrera=2;
+            break;
+case 20705:
+            $carrera=3;
+            break;
+    }
+return $carrera;
 
+}
 
 ?>
